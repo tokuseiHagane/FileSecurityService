9cf5c146a3802df0adf5cc8c10e96cdb2ddc6989
Konstantin V.S <kbc.off@list.ru>
2022-10-05 23:26:27 +0300

    Update FileSecurityService.py

--- FileSecurityService.py
+++ FileSecurityService.py
@@ -50,7 +50,7 @@ class FileSecurityService:
     def __create_file(self):
         pass
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     app = FileSecurityService()
     app.run()
