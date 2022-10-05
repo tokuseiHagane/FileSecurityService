925edad14735c5f5481e4f1aa3755649a22794d1
Konstantin V.S <kbc.off@list.ru>
2022-10-05 23:16:01 +0300

    Базовый скелет

---
+++ FileSecurityService.py
@@ -0,0 +1,56 @@
+# Возможные импорты
+
+
+class FileSecurityService:
+    # Блок отвественности Кирилла
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def __gui_init(self):
+        pass
+
+    # Блок ответственности Константина
+    def __decode_1(self):
+        pass
+
+    def __encode_1(self):
+        pass
+
+    def __decode_2(self):
+        pass
+
+    def __encode_2(self):
+        pass
+
+    def __decode_3(self):
+        pass
+
+    def __encode_3(self):
+        pass
+
+    def __decode_4(self):
+        pass
+
+    def __encode_4(self):
+        pass
+
+    # Блок ответственности Михаила
+    def __get_file(self):
+        pass
+
+    def __read_file(self):
+        pass
+
+    def __write_file(self):
+        pass
+
+    def __create_file(self):
+        pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app = FileSecurityService()
+    app.run()
