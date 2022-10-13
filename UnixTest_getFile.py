714ea341948d5cd9a60688b93e89503e6c0a2b6e
mjkallif <92888620+mjkallif@users.noreply.github.com>
2022-10-13 10:17:58 +0300

    Update UnixTest_getFile.py

--- UnixTest_getFile.py
+++ UnixTest_getFile.py
@@ -7,13 +7,13 @@ class FileSecurityServiceTest(unittest.TestCase):
         self.FileSecurityService = FileSecurityService()
 
     def test_get_file(self):
-        self.assertEqual(self.FileSecurityService.get_file('kok.txt', 'encryption', 'base64'), b'ZG9neQ==')
+        self.assertEqual(self.FileSecurityService.get_file('kok.txt', 'encryption', 'ras', 'aklsndjasnkjd'), b'ZG9neQ==')
 
     def test_read_file(self):
-        self.assertEqual(self.FileSecurityService.read_file('kok.txt'), b'dogy')
+        self.assertEqual(self.FileSecurityService.read_file('kok.txt'), b'helloy world')
 
     def test_write_file(self):
-        self.assertEqual(self.FileSecurityService.write_file(b'helloy world', 'kok.txt', 'encryption', 'base64'), "<_io.BufferedRandom name='files_to_work_with/kok.txt.base64'>")
+        self.assertEqual(self.FileSecurityService.write_file(b'helloy world', 'kok.txt.base64', 'encryption', 'base64'), "<_io.BufferedRandom name='files_to_work_with/kok.txt.base64'>")
 
 
 
