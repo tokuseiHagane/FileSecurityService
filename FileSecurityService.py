a7228ba3525ad5797b14afbfc452e661dcf92419
Константин Соломатин <kbc.off@list.ru>
2022-11-01 23:44:06 +0300

    Update FileSecurityService.py

--- FileSecurityService.py
+++ FileSecurityService.py
@@ -1,3 +1,11 @@
+"""Класс для шифрования/дешифрования файла с ключом или без с помощью RSA, Fernet, base64.
+
+Raises:
+    TypeError: не были переданны байты
+
+Returns:
+    None: Ничего не возвращает
+"""
 import base64
 import binascii
 import hashlib
@@ -9,14 +17,21 @@ from cryptography.fernet import Fernet
 
 
 class FileSecurityService:
-    encoded_password = b''
+    """Класс для шифрования/дешифрования файла с ключом или без с помощью RSA, Fernet, base64.
+
+    Raises:
+        TypeError: не были переданны байты
+
+    Returns:
+        None: Ничего не возвращает
+    """
+
+    enc_password = b''
     public_key, private_key = None, None
 
     # Блок отвественности Кирилла
-    def __init__(self):
-        pass
-
-    def run(self):
+    def run(self) -> None:
+        """Запуск основного приложения."""                      
         while True:
             files_list = self.get_list_of_files()
             print('Файлы в директории: ' + ', '.join(files_list))
@@ -37,7 +52,7 @@ class FileSecurityService:
                 if option == 1:
                     for file in files_list:
                         self.get_file(file, 'base64', 'encryption')
-                    pass  # file_name: str, action: str, tipe_encryption: str, key=None
+                    pass
                 elif option == 2:
                     for file in files_list:
                         self.get_file(file, 'base64', 'decoding')
@@ -74,11 +89,22 @@ class FileSecurityService:
                         self.get_file(file, 'rsa', 'decoding', key)
 
     @staticmethod
-    def get_list_of_files():
-        return [f for f in listdir('./files_to_work_with') if isfile(join('./files_to_work_with', f))]
+    def get_list_of_files() -> list:
+        """Получение списка файлов.
+
+        Returns:
+            list: Список файлов
+        """        
+        directory = './files_to_work_with'
+        return [f for f in listdir(directory) if isfile(join(directory, f))]
 
     @staticmethod
-    def check_rsa_keys():
+    def check_rsa_keys() -> int:
+        """Проверка наличия файлов ключей.
+
+        Returns:
+            int: результат выполнения (0 - успешно, -1 - ошибка)
+        """        
         files = [f for f in listdir('./') if isfile(join('./', f))]
         if 'public.pem' in files and 'private.pem' in files:
             return 0
@@ -88,6 +114,14 @@ class FileSecurityService:
     # Блок ответственности Константина
     @staticmethod
     def decode_base64(to_decode: bytes) -> bytes:
+        """Докодировать из base64.
+
+        Args:
+            to_decode (bytes): байты для декодирования
+
+        Returns:
+            bytes: декодированные файлы
+        """        
         try:
             return base64.b64decode(to_decode)
         except binascii.Error as ascii_e:
@@ -99,11 +133,27 @@ class FileSecurityService:
 
     @staticmethod
     def encode_base64(to_encode: bytes) -> bytes:
+        """Кодирование в base64.
+
+        Args:
+            to_encode (bytes): байты для кодировки в base64
+
+        Returns:
+            bytes: закодированные в base64 байты
+        """        
         return base64.b64encode(to_encode)
 
     def decode_fernet(self, to_decode: bytes) -> bytes:
+        """Декодирование из Fernet.
+
+        Args:
+            to_decode (bytes): байты для декодировки
+
+        Returns:
+            bytes: декодированные байты
+        """        
         try:
-            decoded = Fernet(self.encoded_password).decrypt(to_decode)
+            decoded = Fernet(self.enc_password).decrypt(to_decode)
         except TypeError as te:
             print(f'got not bytes and error "{te}"')
             return b''
@@ -113,8 +163,16 @@ class FileSecurityService:
         return decoded
 
     def encode_fernet(self, to_encode: bytes) -> bytes:
+        """Кодирование в Fernet.
+
+        Args:
+            to_encode (bytes): байты для кодирования
+
+        Returns:
+            bytes: закодированные байты
+        """        
         try:
-            cipher = Fernet(self.encoded_password).encrypt(to_encode)
+            cipher = Fernet(self.enc_password).encrypt(to_encode)
         except TypeError as te:
             print(f'got not bytes and error "{te}"')
             return b''
@@ -123,10 +181,18 @@ class FileSecurityService:
             return b''
         return cipher
 
-    def set_base64_key(self, password: str):
+    def set_base64_key(self, password: str) -> int:
+        """Установка base64 ключа.
+
+        Args:
+            password (str): Ключ для base64
+
+        Returns:
+            int: результат выполнения (0 - успешно, -1 - ошибка)
+        """        
         try:
             key = hashlib.md5(password.encode('utf-8')).hexdigest()
-            self.encoded_password = base64.urlsafe_b64encode(key.encode('utf-8'))
+            self.enc_password = base64.urlsafe_b64encode(key.encode('utf-8'))
         except TypeError as te:
             print(f'got not str and error "{te}"')
             return -1
@@ -135,10 +201,23 @@ class FileSecurityService:
             return -1
         return 0
 
-    def get_base64_key(self):
-        return self.encoded_password
+    def get_base64_key(self) -> bytes:
+        """Экспорт ключа base64.
 
-    def decode_rsa(self, to_decode: bytes):
+        Returns:
+            bytes: ключ base64
+        """        
+        return self.enc_password
+
+    def decode_rsa(self, to_decode: bytes) -> bytes:
+        """Декодирование через RSA.
+
+        Args:
+            to_decode (bytes): байты для декодировки
+
+        Returns:
+            bytes: декодированные байты
+        """        
         try:
             decoded = rsa.decrypt(to_decode, self.private_key)
         except rsa.pkcs1.DecryptionError as de:
@@ -149,7 +228,18 @@ class FileSecurityService:
             return b''
         return decoded
 
-    def encode_rsa(self, to_encode: bytes):
+    def encode_rsa(self, to_encode: bytes) -> bytes:
+        """Кодирование RSA.
+
+        Args:
+            to_encode (bytes): байты для кодировки
+
+        Raises:
+            TypeError: не были переданы байты
+
+        Returns:
+            bytes: закодированные байты
+        """        
         try:
             if type(to_encode) is not bytes or to_encode == b'':
                 raise TypeError
@@ -159,80 +249,147 @@ class FileSecurityService:
             return b''
         return encoded
 
-    def read_rsa_public_key(self, file_path: str):
+    def read_rsa_public_key(self, file_path: str) -> bytes:
+        """Получение публичного ключа RSA.
+
+        Args:
+            file_path (str): путь до файла ключа
+
+        Returns:
+            bytes: полученный ключ
+        """        
         with open(file_path, mode='rb') as f:
             self.public_key = rsa.PublicKey.load_pkcs1(f.read())
         return self.public_key
 
-    def read_rsa_private_key(self, file_path: str):
+    def read_rsa_private_key(self, file_path: str) -> bytes:
+        """Получение приватного ключа RSA.
+
+        Args:
+            file_path (str): путь до файла ключа
+
+        Returns:
+            bytes: полученный ключ
+        """        
         with open(file_path, mode='rb') as f:
             self.private_key = rsa.PrivateKey.load_pkcs1(f.read())
         return self.private_key
 
-    def make_rsa_public_private_key(self):
+    def make_rsa_public_private_key(self) -> list:
+        """Создание публичного и приватного ключа RSA.
+
+        Returns:
+            list: список ключей [публичный, приватный]
+        """        
         self.public_key, self.private_key = rsa.newkeys(512)
         return [self.public_key, self.private_key]
 
-    def save_rsa_public_key(self):
-        with open('public.pem', mode='wb+') as f:
-            f.write(self.public_key.save_pkcs1())
+    def save_rsa_public_key(self) -> int:
+        """Сохранение публичного ключа в файл.
+
+        Returns:
+            int: результат выполнения (0 - успешно, -1 - ошибка)
+        """        
+        try:
+            with open('public.pem', mode='wb+') as f:
+                f.write(self.public_key.save_pkcs1())
+        except Exception as e:
+            return -1
         return 0
 
-    def save_rsa_private_key(self):
-        with open('private.pem', mode='wb+') as f:
-            f.write(self.private_key.save_pkcs1())
+    def save_rsa_private_key(self) -> int:
+        """Сохранение приватного ключа в файл.
+
+        Returns:
+            int: результат выполнения (0 - успешно, -1 - ошибка)
+        """       
+        try:
+            with open('private.pem', mode='wb+') as f:
+                f.write(self.private_key.save_pkcs1())
+        except Exception as e:
+            return -1
         return 0
 
     # Блок ответственности Михаила
-    def get_file(self, file_name: str, action: str, tipe_encryption: str, key=None):
+    def get_file(self, file_name: str, action: str, encryption: str, key: bytes=None) -> bytes:
+        """Получение данных из файла.
+
+        Args:
+            file_name (str): имя файла для обработки
+            action (str): encode или decode (кодировать, декодировать)
+            encryption (str): тип кодирования/декодирования
+            key (bytes, optional): Ключ для base64. По умолчанию None.
+
+        Returns:
+            bytes: _description_
+        """              
         f = self.read_file(file_name)
         fil = None
-        if action == 'encryption':
-            if tipe_encryption == 'rsa':
+        if action == 'encode':
+            if encryption == 'rsa':
                 self.read_rsa_public_key('./public.pem')
                 self.read_rsa_private_key('./private.pem')
                 fil = self.encode_rsa(f)
-            if tipe_encryption == 'fernet':
+            if encryption == 'fernet':
                 self.set_base64_key(key)
                 fil = self.encode_fernet(f)
-            if tipe_encryption == 'base64':
+            if encryption == 'base64':
                 fil = self.encode_base64(f)
                 print(fil)
-        if action == 'decoding':
-            if tipe_encryption == 'rsa':
+        elif action == 'decode':
+            if encryption == 'rsa':
                 self.read_rsa_public_key('./public.pem')
                 self.read_rsa_private_key('./private.pem')
                 fil = self.decode_rsa(f)
-            if tipe_encryption == 'fernet':
+            if encryption == 'fernet':
                 self.set_base64_key(key)
                 fil = self.decode_fernet(f)
-            if tipe_encryption == 'base64':
+            if encryption == 'base64':
                 fil = self.decode_base64(f)
         return fil
 
     @staticmethod
-    def read_file(file_name):
+    def read_file(file_name: str) -> bytes:
+        """Получение файла для кодирования/декодирования.
+
+        Args:
+            file_name (str): _имя файла
+
+        Returns:
+            bytes: данные из файла
+        """        
         path = f'files_to_work_with/{file_name}'
         with open(mode='r+b', file=path) as f:
             file_data = f.read()
         return file_data
 
     @staticmethod
-    def write_file(fil, file_name: str, action, tipe_encryption):
-        file_date = None
-        if action == 'encryption':
-            path = f'files_to_work_with/{file_name}.{tipe_encryption}'
-            file_date = open(mode='w+b', file=path)
-            file_date.write(fil)
-            file_date.close()
-        if action == 'decoding':
-            f = file_name.split('.')
-            f = '.'.join(f[:-1])
-            path = f'files_to_work_with/{f}'
-            file_date = open(mode='w+b', file=path)
-            file_date.write(fil)
-            file_date.close()
-        return str(file_date)
+    def write_file(data: bytes, file_name: str, action: str, encryption: str) -> int:
+        """Запись зашифрованного/расшифрованного файла.
+
+        Args:
+            data (bytes): данные для шифрования
+            file_name (str): имя файла
+            action (str): encode или decode (кодировать, декодировать)
+            encryption (str): тип кодирования/декодирования
+
+        Returns:
+            int: результат выполнения (0 - успешно, -1 - ошибка)
+        """        
+        try:
+            if action == 'encode':
+                path = f'files_to_work_with/{file_name}.{encryption}'
+                with open(file=path, mode='w+b') as f:
+                    f.write(data)
+            elif action == 'decode':
+                name = '.'.join(file_name.split('.')[:-1])
+                path = f'files_to_work_with/{name}'
+                with open(file=path, mode='w+b') as f:
+                    f.write(data)
+        except Exception as e:
+            print(str(e))
+            return -1
+        return 0
 
 
 if __name__ == '__main__':
